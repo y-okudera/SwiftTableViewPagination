d9ffc94fe5d464d94a846cbeefafe573d0dda34e
yuoku <appledev.yuoku@gmail.com>
2021-12-08 00:46:46 +0900

    Create a new project.

---
+++ SwiftTableViewPagination/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftTableViewPagination
+//
+//  Created by Yuki Okudera on 2021/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
